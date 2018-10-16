2eed531d538749c199862500ce90a5e5c7c6d8cd
Éderson Lehugeur <ederson-lehugeur@tjmrs.jus.br>
2018-10-16 14:39:57 -0300

    Correção de erros

--- src/main/java/br/com/blog/controller/PostController.java
+++ src/main/java/br/com/blog/controller/PostController.java
@@ -1,19 +1,18 @@
 package br.com.blog.controller;
 
-import java.util.Optional;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.blog.model.Post;
 import br.com.blog.service.PostService;
 
+@Controller
 public class PostController {
 	@Autowired
 	private PostService service;
@@ -26,8 +25,15 @@ public class PostController {
 		return mv;
 	}
 
+	@GetMapping("/hello")
+	public ModelAndView hello() {
+		ModelAndView mv = new ModelAndView("/hello");
+
+		return mv;
+	}
+
 	@GetMapping("/add")
-	public ModelAndView add(@Valid Post post) {
+	public ModelAndView add(Post post) {
 		ModelAndView mv = new ModelAndView("/postAdd");
 		mv.addObject("post", post);
 
@@ -49,7 +55,7 @@ public class PostController {
 	}*/
 
 	@PostMapping("/save")
-	public ModelAndView save(@Valid Post post, BindingResult result) {
+	public ModelAndView save(Post post, BindingResult result) {
 		if(result.hasErrors()) {
 			return add(post);
 		}
--- src/main/java/br/com/blog/model/Post.java
+++ src/main/java/br/com/blog/model/Post.java
@@ -42,4 +42,44 @@ public class Post implements Serializable {
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	@NotNull(message = "Data é uma informação obrigatória.")
 	private Date data;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
 }
