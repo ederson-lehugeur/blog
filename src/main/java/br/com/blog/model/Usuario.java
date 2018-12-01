6aafbbf6eea98c0110cce87a4f9d25a46f1d2e20
Éderson Lehugeur <eder.lehugeur@gmail.com>
2018-12-01 14:29:31 -0200

    Inclusão do Font Awesome 4.7.0 e correções nos models

--- src/main/java/br/com/blog/model/Usuario.java
+++ src/main/java/br/com/blog/model/Usuario.java
@@ -14,7 +14,7 @@ public class Usuario implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Column(nullable = false, length = 80)
